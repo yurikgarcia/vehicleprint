f4f1217e070dbf516bd984e502839f300156ce99
yurikgarcia <90262671+yurikgarcia@users.noreply.github.com>
2022-11-18 19:02:30 -0500

    form that prints

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import React, { useRef, useState } from "react";
+import ReactToPrint from "react-to-print";
+import Button from '@mui/material/Button';
+
+
+
+export const ThemeContext = React.createContext();
+
+export default function App() {
+
+  const [vehicle, setVehicle] = useState({
+    make: '',
+    model: '',
+    plate: '',
+  });
+
+    let componentRef = useRef();
+
+
+
+    console.log(vehicle);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ThemeContext.Provider value={vehicle}>
+
+      <div id="print_component">
+        {/* button to trigger printing of target component */}
+        <ReactToPrint
+          trigger={() => <Button>Print this out!</Button>}
+          content={() => componentRef}
+          />
+
+        {/* component to be printed */}
+        <div style={{ display: "none" }}>
+          <ComponentToPrint ref={(el) => (componentRef = el)} />
+        </div>
+
+        <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+      >
+    
+        <TextField
+          id="outlined-error-helper-text"
+          label="Make"
+          onChange={(e) => setVehicle({ ...vehicle,  make: e.target.value })}
+          />
+        <TextField
+          id="outlined-error-helper-text"
+          label="Model"
+          onChange={(e) => setVehicle({ ...vehicle, model: e.target.value })}
+          />
+        <TextField
+          id="outlined-error-helper-text"
+          label="Plate"
+          onChange={(e) => setVehicle({ ...vehicle, plate: e.target.value })}
+          />
+    </Box>
+    <h1>{vehicle.make}</h1>
+    <h1>{vehicle.model}</h1>
+    <h1>{vehicle.plate}</h1>
+      </div>
+          </ThemeContext.Provider>
+    </>
   );
 }
 
-export default App;
+// component to be printed
+//pass the variable name vehicle to the component
+
+
+
+
+class ComponentToPrint extends React.Component {
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {vehicle => (
+          <div>
+        {vehicle.make}
+        {vehicle.model}
+        {vehicle.plate}
+          </div>
+        )}
+      </ThemeContext.Consumer>
+
+
+    );
+  }
+}
